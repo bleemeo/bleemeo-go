b7003f66f1c153dbb3ee34d70bb3d4d14d5b5658
Thomas Delbende <thomas.delbende@bleemeo.com>
2025-02-14 13:49:31 +0100

    chore: update enums

--- enums.go
+++ enums.go
@@ -34,6 +34,7 @@ const (
 	AgentType_vSphereCluster     AgentType = "vsphere_cluster"
 	AgentType_vSphereHost        AgentType = "vsphere_host"
 	AgentType_vSphereVM          AgentType = "vsphere_vm"
+	AgentType_Application        AgentType = "application"
 )
 
 type DisconnectionReason int
